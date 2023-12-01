f2e85e7768dabfbc59c50c52154f0f8d3cfa44b8
Matt Goodrich <mgoodrich@foreflight.com>
2023-12-01 09:49:22 -0500

    Init

--- run.py
+++ run.py
@@ -26,12 +26,21 @@ if __name__ == "__main__":
     parser.add_argument("--year", "-y", type=int, help="The year to run.", default=now.year)
     parser.add_argument("--day", "-d", type=int, help="The day to run.", default=now.day)
     parser.add_argument("--extra", "-e", help="Choose a different solution to run.")
+    parser.add_argument("--sampleonly", "-s", action='store_true', help="Flag to only run sample.")
+    parser.add_argument("--secondsample", "-ss", action='store_true', help="Flag to use sample2.")
     args = parser.parse_args()
 
     input_paths = {
         "sample": f"input/{args.year}/day{args.day:02}_sample.txt",
         "input": f"input/{args.year}/day{args.day:02}.txt",
     }
+
+    if (args.secondsample):
+        newsample = {
+            "sample": f"input/{args.year}/day{args.day:02}_sample2.txt"
+        }
+        input_paths.update(newsample)
+        print(input_paths)
 
     if not os.path.exists(input_paths["input"]):
         try:
@@ -57,7 +66,9 @@ if __name__ == "__main__":
             continue
         print(f"--- {i} ---")
         print("sample:", end="\t")
-        run(getattr(module, i), f"input/{args.year}/day{args.day:02}_sample.txt")
-        reload(module)
-        print("input:", end="\t")
-        run(getattr(module, i), f"input/{args.year}/day{args.day:02}.txt")
+        run(getattr(module, i), input_paths["sample"])
+
+        if not args.sampleonly:
+            reload(module)
+            print("input:", end="\t")
+            run(getattr(module, i), input_paths["input"])
